891b2bb96e54c0da0bc762bf70288602bdd6a6a7
Suneel <suneel.jakka@xandr.com>
2020-12-11 12:45:01 -0600

    First commit

---
+++ recursion/lettercasepermutewithmutable784.py
@@ -0,0 +1,28 @@
+class solution(object):
+
+    def lettercasepermutewithmutable784(self, inpstr):
+        res = []
+
+        def helper(inpstr, level, slate):
+            if level == len(inpstr):
+                res.append("".join(slate))
+                return
+            else:
+                if inpstr[level].isdigit():
+                    slate.append(inpstr[level])
+                    helper(inpstr, level+1, slate)
+                    slate.pop()
+                else:
+                    slate.append(inpstr[level].lower())
+                    helper(inpstr, level + 1, slate)
+                    slate.pop()
+                    slate.append(inpstr[level].upper())
+                    helper(inpstr, level + 1, slate)
+                    slate.pop()
+
+        helper(inpstr, 0, [])
+        return res
+
+
+sol = solution()
+print(sol.lettercasepermutewithmutable784("abc1"))
